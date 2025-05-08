b22399de696cb5688d515055cda21dc871801e5c
prakhar sinha <prakharkumarsinha8@gmail.com>
2025-05-08 10:39:02 +0530

    Remove node_modules and add it to .gitignore

--- init/index.js
+++ init/index.js
@@ -17,6 +17,7 @@ async function main(){
 
 const initDB = async ()=>{
     await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({...obj, owner: "68172c421da82c14782c1884"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
---
+++ routes/listing.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn, isOwner,validateList} = require("../middleware.js");
+
+const ListingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
+
+router.route("/")
+.get(wrapAsync(ListingController.index ))
+.post(isLoggedIn,upload.single('listing[image]'),validateList,
+   wrapAsync(ListingController.createlisting))
+   
+ //New Route
+ router.get("/new" ,isLoggedIn, ListingController.renderNewForm)
+
+router.route("/:id")   
+.get( wrapAsync(ListingController.showlisting))
+.put(isLoggedIn,isOwner,upload.single('listing[image]'),validateList,
+   wrapAsync(ListingController.updateListing))
+.delete(isLoggedIn,isOwner,wrapAsync(ListingController.destroyListing));
+
+
+
+ 
+ //Edit Route
+ router.get("/:id/edit" ,isLoggedIn,isOwner,wrapAsync(ListingController.renderEditForm ))
+ 
+ module.exports = router;
